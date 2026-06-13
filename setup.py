a06456431a797fbc5fb370233924a2c916639bc2
yaqoobgmt26@gmail.com <97838251+yaqoobgmt@users.noreply.github.com>
2026-06-13 13:14:35 +0530

    Project Structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Mohmad Yaqoob",
+    author_email="yaqoobgmt26@gmail.com",
+    packages=find_packages()
+)
